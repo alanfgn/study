2432dbbc044a74f175808d70f2ce70c2d46ea2ae
Alan Souza Fagundes <alan.souza@solutis.com.br>
2021-04-28 13:04:28 -0300

    scala 1

---
+++ scala/Foto.java
@@ -0,0 +1,18 @@
+public class Foto {
+    private final String id;
+    private final String ower;
+
+    public Foto(String id, String ower) {
+        this.id = id;
+        this.ower = ower;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getOwer() {
+        return this.ower;
+    }
+    
+}
